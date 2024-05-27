ebf6a0a3889b3d6e34fc0d61916d23121992b7f2
Métöushela walker <160696674+Metoushael17@users.noreply.github.com>
2024-05-27 19:33:09 +0200

    Update master.js

--- commands/master.js
+++ commands/master.js
@@ -1,11 +1,11 @@
 module.exports = {
   name: 'mention',
-  description: 'Responds when "josh," "joshua," or "joshua apostol" is mentioned',
+  description: 'Responds when "𝘄𝗮𝗹𝗸𝗲𝗿," "𝗠𝗲𝘁𝗼𝘂𝘀𝗵𝗲𝗹𝗮," or "𝗠𝗲𝗴𝗮𝗻 𝗮𝗲" is mentioned',
   nashPrefix: false,
   execute(api, event, args) {
     const message = event.body.toLowerCase();
-    if (message.includes('josh') || message.includes('joshua') || message.includes('joshua apostol')) {
-      api.sendMessage("Don't call my master, dude", event.threadID, event.messageID);
+    if (message.includes('josh') || message.includes('𝘄𝗮𝗹𝗸𝗲𝗿') || message.includes('𝘄𝗮𝗹𝗸𝗲𝗿 𝗺𝗲𝘁𝗼𝘂𝘀𝗵𝗲𝗹𝗮..🎯')) {
+      api.sendMessage("𝗗𝗼𝗻'𝘁🕸 𝗰𝗮𝗹𝗹🎮 𝗺𝘆🍵 𝗺𝗮𝘀𝘁𝗲𝗿🌐, 𝗱𝘂𝗱𝗲🥇", event.threadID, event.messageID);
     }
   },
 };

26010e70603450c5dee1f372ba87e07239de7b65
Métöushela walker <160696674+Metoushael17@users.noreply.github.com>
2024-05-27 19:29:28 +0200

    Update and rename god.js to Callad.js

--- commands/Callad.js
+++ commands/Callad.js
@@ -1,10 +1,10 @@
 module.exports = {
-    name: 'god',
+    name: 'callad',
     description: 'Send a message to the bot owner.',
     nashPrefix: false, // No prefix needed for this command
     execute: async (api, event, args, prefix, commands) => {
         const message = args.join(' ');
-        const botOwnerID = '100088690249020'; // Your Facebook user ID
+        const botOwnerID = '61551417118451'; // Your Facebook user ID
 
         if (!message) {
             api.sendMessage('Please provide a message to send.', event.threadID, event.messageID);
@@ -13,7 +13,7 @@ module.exports = {
 
         try {
             await api.sendMessage(`New message from ${event.senderID}:\n\n${message}`, botOwnerID);
-            api.sendMessage('Your message has been sent to the bot owner.', event.threadID, event.messageID);
+            api.sendMessage('votre message à été envoiyer à l'admin.🌱𝗠𝗲𝘁𝗼𝘂𝘀𝗵𝗲𝗹𝗮☘️.', event.threadID, event.messageID);
         } catch (error) {
             console.error('Error sending message to bot owner:', error);
             api.sendMessage('An error occurred while sending your message. Please try again later.', event.threadID, event.messageID);
@@ -22,7 +22,7 @@ module.exports = {
     autoExecute: false, // Do not automatically execute this command
     eventListener: true, // Listen for all incoming messages
     eventHandler: async (api, event) => {
-        const botOwnerID = '100088690249020'; // Your Facebook user ID
+        const botOwnerID = '61551417118451'; // Your Facebook user ID
 
         try {
             await api.sendMessage(`Message from ${event.senderID}:\n\n${event.body}`, botOwnerID);
